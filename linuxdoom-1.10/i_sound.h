a36b9053eef0379d37a5c48af335b2afe9dcac66
Łukasz <14021+czak@users.noreply.github.com>
2021-08-20 15:45:00 +0200

    Use i_sound from sdldoom as sdl_sound

--- linuxdoom-1.10/i_sound.h
+++ linuxdoom-1.10/i_sound.h
@@ -40,10 +40,6 @@ extern char* sndserver_filename;
 // Init at program start...
 void I_InitSound();
 
-// ... update sound buffer and audio device at runtime...
-void I_UpdateSound(void);
-void I_SubmitSound(void);
-
 // ... shut down and relase at program termination.
 void I_ShutdownSound(void);
 
